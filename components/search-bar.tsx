403b9a84fb4a816588483dbf8c0809fa5c1c9451
Francesco Dammacco <francescodammacco@live.it>
2024-03-21 01:15:50 +0000

    chore: boilerplate cleanup

---
+++ components/search-bar.tsx
@@ -0,0 +1,20 @@
+import { SearchIcon } from "@/components/icons";
+import { Input } from "@nextui-org/input";
+
+export const SearchBar = () => {
+  return (
+    <Input
+      aria-label="Search"
+      classNames={{
+        inputWrapper: "bg-default-100",
+        input: "text-sm",
+      }}
+      labelPlacement="outside"
+      placeholder="Search..."
+      startContent={
+        <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
+      }
+      type="search"
+    />
+  );
+};
