bc90a22cf0886d609b85ef0d6d8df215a21b05af
Francesco Dammacco <francescodammacco@live.it>
2024-03-25 03:41:12 +0000

    refactor: ux/ui improvements

--- src/components/detail-sidebar.tsx
+++ src/components/detail-sidebar.tsx
@@ -18,29 +18,27 @@ export const DetailSidebar = ({
   creditsProps,
 }: DetailSidebarProps) => {
   return (
-    <>
-      <Card className="sticky bottom-0 z-10 w-full h-fit self-center lg:w-[28rem] animate-fade-up lg:animate-fade-left rounded-b-none lg:rounded-b-large">
-        <CardHeader className="flex-col items-start gap-4">
-          <div className="flex flex-row justify-between w-full gap-2">
-            <Credits {...creditsProps} />
-            <CommentsModal />
-            <FavouriteButton photoId={id} />
-          </div>
-          {description && (
-            <p className="italic text-sm">&quot;{description}&quot;</p>
-          )}
-        </CardHeader>
-        <Divider />
+    <Card className="sticky bottom-0 z-10 w-full h-fit self-center lg:w-[28rem] animate-fade-up lg:animate-fade-left rounded-b-none lg:rounded-b-large">
+      <CardHeader className="flex-col items-start gap-4 pb-9 lg:pb-3">
+        <div className="flex flex-row justify-between w-full gap-2">
+          <Credits {...creditsProps} />
+          <CommentsModal />
+          <FavouriteButton photoId={id} />
+        </div>
+        {description && (
+          <p className="italic text-sm">&quot;{description}&quot;</p>
+        )}
+      </CardHeader>
+      <Divider />
 
-        <CardBody className="hidden lg:flex h-96">
-          <CommentsList />
-        </CardBody>
-        <Divider className="hidden lg:flex" />
+      <CardBody className="hidden lg:flex h-96">
+        <CommentsList />
+      </CardBody>
+      <Divider className="hidden lg:flex" />
 
-        <CardFooter className="hidden lg:flex">
-          <CommentInput />
-        </CardFooter>
-      </Card>
-    </>
+      <CardFooter className="hidden lg:flex">
+        <CommentInput />
+      </CardFooter>
+    </Card>
   );
 };
